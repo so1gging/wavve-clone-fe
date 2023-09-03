2f565b0af509524b7cfeab339e2de12a0de16353
김솔지 <so1gging@gmail.com>
2023-09-03 17:47:53 +0900

    :lipstick: Typography CssProperties 추가

--- components/Typography/Typography.tsx
+++ components/Typography/Typography.tsx
@@ -1,15 +1,16 @@
 import { typography } from '@/emotion'
 import styled from '@emotion/styled'
 import { ElementType, PropsWithChildren } from 'react'
+import { CSSProperties } from '@emotion/serialize'
 
-interface TypographyProps extends PropsWithChildren {
+interface TypographyProps extends PropsWithChildren, CSSProperties {
   variants: typography
   as?: ElementType
 }
 
-export default function Typography({ variants, as, children }: TypographyProps) {
+export default function Typography({ variants, as, children, ...styleProps }: TypographyProps) {
   return (
-    <Styled variants={variants} as={as}>
+    <Styled variants={variants} as={as} css={{ ...styleProps }}>
       {children}
     </Styled>
   )
