fd93ac607b8eeea3cc476fee429ecf59789d7f2a
김솔지 <so1gging@gmail.com>
2023-09-03 16:47:59 +0900

    :lipstick: notoSan font

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,27 @@
 import StyledJsxRegistry from '@/app/registry'
 import { ThemeProvider } from '@emotion/react'
 import { theme } from '@/libs/styles/theme'
+import { Noto_Sans_KR } from 'next/font/google'
+
+const notoSansKr = Noto_Sans_KR({
+  weight: ['100', '300', '400', '500', '700', '900'],
+  subsets: ['latin'],
+  fallback: [
+    '-apple-system',
+    'Malgun Gothic',
+    'Apple SD Gothic Neo',
+    'Roboto',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol',
+    'sans-serif',
+  ],
+})
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className={notoSansKr.className}>
         <StyledJsxRegistry>
           <ThemeProvider theme={theme}>{children}</ThemeProvider>
         </StyledJsxRegistry>
