0bbd79e0b97eb78b3506218b3ca1efa00ca42fe8
김솔지 <so1gging@gmail.com>
2023-09-03 18:04:19 +0900

    :lipstick: Divider

---
+++ components/Divider/Divider.tsx
@@ -0,0 +1,11 @@
+import { CSSProperties } from '@emotion/serialize'
+import { theme } from '@/libs/styles/theme'
+
+interface DividerProps {
+  width?: CSSProperties['width']
+  height?: CSSProperties['height']
+  color?: CSSProperties['color']
+}
+export default function Divider({ width = '100%', height = '1px', color = theme.palette.gray }: DividerProps) {
+  return <div css={{ width, height, backgroundColor: color }} />
+}
