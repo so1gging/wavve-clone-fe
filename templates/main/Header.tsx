2b1731ed849d11e98730053370d7944c2477601b
김솔지 <so1gging@gmail.com>
2023-09-06 19:59:43 +0900

    :lipstick: Header Area 로 교체

--- templates/main/Header.tsx
+++ templates/main/Header.tsx
@@ -1,20 +1,15 @@
 'use client'
-import { css } from '@emotion/react'
 import Nav from '@/templates/main/Nav'
 import Divider from '@/components/Divider/Divider'
 import Gnb from '@/templates/main/Gnb'
+import Area from '@/components/layout/Area'
 
 export default function Header() {
   return (
-    <div
-      css={css({
-        display: 'flex',
-        flexDirection: 'column',
-      })}
-    >
+    <Area>
       <Nav />
       <Divider />
       <Gnb />
-    </div>
+    </Area>
   )
 }
